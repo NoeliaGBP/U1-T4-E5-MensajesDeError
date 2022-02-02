de8aba81474e1c83835d6a5a0ad60b50a5f6cc66
Noelia Bahena <74998452+NoeliaGBP@users.noreply.github.com>
2022-02-01 22:48:06 -0600

    Inicio del proyecto

---
+++ errormessages/src/test/java/mx/edu/utez/errormessages/ErrormessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.errormessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrormessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
