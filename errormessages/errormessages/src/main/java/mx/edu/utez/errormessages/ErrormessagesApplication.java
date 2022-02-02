1026ebecaf286c47109bcf70de6221f138ba044b
Noelia Bahena <74998452+NoeliaGBP@users.noreply.github.com>
2022-02-01 22:46:44 -0600

    Inicio del proyecto

---
+++ errormessages/errormessages/src/main/java/mx/edu/utez/errormessages/ErrormessagesApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.errormessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrormessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrormessagesApplication.class, args);
+	}
+
+}
