f4184641a7183f59fd38b71c4ca878f14e8b116f
Athziri Fdz <20193tn009@utez.edu.mx>
2022-02-13 13:48:01 -0600

    Agregando user.js

---
+++ errormessages/src/main/resources/static/js/control/user/user.js
@@ -0,0 +1,83 @@
+app.controller('user', function ($rootScope, $scope, Main, $http, $location, $localStorage) {
+
+
+    $scope.users = [];
+    $scope.updateUser = {};
+    $scope.disableUser = {};
+    $scope.user = {};
+
+    let us = '/api/user';
+
+    $scope.init = () => {
+        Main.get(us,
+            (success) => {
+                $scope.users = success.data.result;
+            },
+            (error) => {
+                $rootScope.showToastr(error.data.type, error.data.text);
+            });
+    }
+
+    $scope.showModalUpdate = (user) => {
+        angular.copy(user, $scope.updateUser);
+    }
+
+    $scope.showModalDisable = (user) => {
+        angular.copy(user, $scope.disableUser);
+    }
+
+    $scope.save = () => {
+        Main.post(us, $scope.user,
+            (success) => {
+                $rootScope.showToastr(success.data.type, success.data.text);
+                $scope.users.push(success.data.result);
+                $scope.user = {};
+            }, (error) => {
+                $rootScope.showToastr(error.data.type, error.data.text);
+                $scope.clear2 = {};
+                $scope.user = angular.copy($scope.clear2);
+                document.getElementById('createForm').reset();
+            })
+    }
+
+
+    $scope.update = () => {
+        Main.put(us, $scope.updateUser,
+            (success) => {
+                $rootScope.showToastr(success.data.type, success.data.text);
+                let result = success.data.result;
+                let index = $scope.users.findIndex(x => x.id === result.id);
+                $scope.users.splice(index, 1, result);
+            },
+            (error) => {
+                $rootScope.showToastr(error.data.type, error.data.text);
+            });
+    }
+
+
+    $scope.disable = () => {
+        $rootScope.loading = true;
+        Main.put(us + '/' + $scope.disableUser.id,null,
+            (success) => {
+                $rootScope.showToastr(success.data.type, success.data.text);
+                let result = success.data.result;
+                let index = $scope.users.findIndex(x => x.id === result.id);
+                $scope.users.splice(index, 1, result);
+            },
+            (error) => {
+                $("#switch" + $scope.disableUser.id).prop('checked', !$("#switch" + $scope.disableUser.id).prop('checked'));
+                $rootScope.showToastr(error.data.type, error.data.text);
+            });
+    }
+
+    $scope.cancelDisabled = () => {
+        $("#switch" + $scope.disableUser.id).prop('checked', !$("#switch" + $scope.disableUser.id).prop('checked'));
+    }
+
+    $scope.clear = () => {
+        $scope.clear2 = {};
+        $scope.user = angular.copy($scope.clear2);
+        document.getElementById('createForm').reset();
+    }
+
+});
